410763827fad2940407a6d834aea6c8479af0e23
Karthik Mohan <karthikeyan.mohan@applaudhr.com>
2025-08-13 18:35:04 +0530

    Add HTML reporter to Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
-    ['allure-playwright']
+    ['allure-playwright'],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
